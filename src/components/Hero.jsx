523d3a25082c773adf84ea7f859f4afc8eb5429e
Muhammad Ali <muhammadalimandela01@gmail.com>
2024-09-09 18:20:24 +0530

    hero added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from 'next/image'
-import {useState} from 'react'; 
+import {useEffect, useState} from 'react'; 
 import { NextIcon, PrevIcon } from './SliderIcons';
 import Link from 'next/link';
 import { SliderItems } from '@/shared/itemsSlider';
@@ -17,6 +17,13 @@ const HeroComponent = () => {
 function handleNext() {
     setcurrent((prev) => (prev === length - 1 ? 0 : prev + 1));
 }
+useEffect(() => {
+  let intervalId = setInterval(() => {
+      setcurrent((prev) => (prev === length - 1 ? 0 : prev + 1));
+  }, 4000);
+
+  return () => clearInterval(intervalId);   
+}, [length]);  
 
   return ( 
     <> <div className='hidden md:block h-[70vh] relative'> 
@@ -29,15 +36,14 @@ function handleNext() {
     
 </div>
 {/* mobile */}
-   <div className=' md:hidden h-[30vh] relative'> 
-   <MobileSlider/>
-          {/* <Slider item={items[current]}/>
-          <div className=' absolute flex gap-8 items-center right-2 bottom-2'>
-          <PrevIcon onHandle={handlePrev} color='#800000'/>
-              <span className='w-8 h-8 border-2 border-lh font-bold text-lh rounded-full flex items-center justify-center'>{current+1}</span>
-            <NextIcon onHandle={handleNext} color='#800000'/>
+   <div className=' md:hidden w-full bg-neutral-300 shadow-md h-[30vh] relative'> 
+   <MobileSlider item={items[current]}/>
+   <div className=' absolute flex gap-8 items-center right-[30%] bottom-1'>
+          <PrevIcon size={22} onHandle={handlePrev} color='grey'/>
+              <span className='w-6 h-6 text-base border-2 border-[grey] font-bold text-[grey] rounded-full flex items-center justify-center'>{current+1}</span>
+            <NextIcon size={22} onHandle={handleNext} color='grey'/>
           </div>
-           */}
+                 
     </div>
 </>
   )
@@ -62,12 +68,28 @@ export const Slider=({item})=>{
 }
 
 
-export const MobileSlider=()=>{
-  return(
-    <div className='w-full h-full bg-slate-500'>
-ggg
+export const MobileSlider = ({ item }) => {
+  const backgroundImageStyle = {
+    backgroundImage: `url(${item?.src})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center', 
+    color: 'white',
+  };
+
+  return (
+    <div className='w-full h-full'>
+      <div style={backgroundImageStyle} className="h-full w-full relative ">
+        <p className='p-2 bg-black '>{item?.label}</p>
+        <div className=' p-2 absolute bottom-0 w-full  items-center'>
+            <h1 className=' text-black text-lg  font-semibold'>{item?.title}</h1>
+            <button className='bg-lh w-max font-light py-1 px-4 mt-1 hover:text-white hover:bg-slate-900 transition-all ease-in-out duration-400 text-white rounded-full '>
+             <Link href={'/'}>Shop now</Link>
+         </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
 
 export default HeroComponent
